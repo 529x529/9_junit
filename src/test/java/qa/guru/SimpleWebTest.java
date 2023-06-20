18ac5d592850d1eb9e57d7f86843b3448b9df32b
mikhailrezchikov <mrezchikov529@gmail.com>
2023-06-20 22:09:28 +0300

    first commit

---
+++ src/test/java/qa/guru/SimpleWebTest.java
@@ -0,0 +1,24 @@
+package qa.guru;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleWebTest {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.pageLoadStrategy = "eager";
+    }
+    
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
